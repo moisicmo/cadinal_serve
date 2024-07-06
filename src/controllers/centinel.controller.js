a79bbc8d6011e3ea122d978afe568a69231d94dc
moisicmo <moisic.mo@gmail.com>
2024-07-06 12:01:40 -0400

    cambios

--- src/controllers/centinel.controller.js
+++ src/controllers/centinel.controller.js
@@ -5,33 +5,34 @@ var crypto = require('crypto');
 
 const processPayment = async (req = request, res = response) => {
   try {
-
-    const DFReferenceId = req.body.DFReferenceId;
-    const OrgUnitId = '';
+    console.log(req.body);
+    const {DFReferenceId, card, customer } = req.body;
     // Generar el timestamp en milisegundos
     const timestamp = Math.round(Date.now());
+    // referenceId
+
     // Valores proporcionados
+    const OrgUnitId = '';
     const apiKey = '';
     const identifier = '';
+
+
     // Crear la cadena a hashear
     const preHash = timestamp + apiKey;
-    // Crear el hash SHA-256
     const hashed = crypto.createHash('sha256').update(preHash).digest();
-    // Codificar en base64
     const signature = hashed.toString('base64');
 
     console.log('Timestamp:', timestamp);
     console.log('Signature:', signature);
-    console.log('DFReferenceId:', DFReferenceId);
 
     // Construir el XML
     const root = create({ version: '1.0' })
       .ele('CardinalMPI')
         .ele('MsgType').txt('cmpi_lookup').up()
+        .ele('DeviceChannel').txt('sdk').up()
         .ele('Version').txt('1.7').up()
         .ele('TransactionType').txt('C').up()
         .ele('TransactionMode').txt('S').up()
-        .ele('DeviceChannel').txt('SDK').up()
         .ele('Algorithm').txt('SHA-256').up()
         //
         .ele('DFReferenceId').txt(DFReferenceId).up()
@@ -41,42 +42,40 @@ const processPayment = async (req = request, res = response) => {
         // 
         .ele('Signature').txt(signature).up()
         .ele('Timestamp').txt(timestamp).up()
-        .ele('OrderNumber').txt('order-0001').up()
         .ele('Amount').txt('1000').up()
-        .ele('CurrencyCode').txt('840').up()
         // card
-        .ele('CardNumber').txt('4000000000002503').up()
-        .ele('CardExpMonth').txt('01').up()
-        .ele('CardExpYear').txt('2026').up()
+        .ele('CardExpMonth').txt(card.expirationMonth).up()
+        .ele('CardExpYear').txt(`20${card.expirationYear}`).up()
+        .ele('CardNumber').txt(card.number).up()
+        .ele('CurrencyCode').txt('840').up()
+        .ele('Email').txt(customer.email).up()
+        .ele('MobilePhone').txt('+59173735766').up()
+        .ele('OrderNumber').txt('order-0001').up()
         // billing
-        .ele('BillingAddress1').txt('Ludovico ').up()
-        .ele('BillingCity').txt('Mentor').up()
+        .ele('BillingAddress1').txt(customer.address1).up()
+        .ele('BillingAddress2').txt(customer.address2).up()
+        .ele('BillingCity').txt(customer.locality).up()
         .ele('BillingCountryCode').txt('840').up()
-        .ele('BillingFirstName').txt('test1').up()
-        .ele('BillingLastName').txt('').up()
-        .ele('BillingPhone').txt('').up()
-        .ele('BillingPostalCode').txt('44060').up()
+        .ele('BillingFirstName').txt(customer.firstName).up()
+        .ele('BillingFullName').txt(`${customer.firstName} ${customer.lastName}`).up()
+        .ele('BillingLastName').txt(customer.lastName).up()
+        .ele('BillingPostalCode').txt(customer.postalCode).up()
         .ele('BillingState').txt('OH').up()
         // shipping
-        .ele('ShippingAddress1').txt('').up()
+        .ele('ShippingAddress1').txt('8100 Tyler Blvd').up()
+        .ele('ShippingAddress2').txt('').up()
         .ele('ShippingCity').txt('44060').up()
         .ele('ShippingCountryCode').txt('840').up()
-        .ele('ShippingFirstName').txt('MOI').up()
-        .ele('ShippingLastName').txt('as').up()
-        .ele('ShippingMethodIndicator').txt('').up()
         .ele('ShippingPostalCode').txt('44060').up()
         .ele('ShippingState').txt('OH').up()
-        .ele('ShippingPhone').txt('').up()
-        // .ele('ShippingAmount').txt('').up()
-        .ele('MobilePhone').txt('').up()
-        .ele('Email').txt('').up()
+        
         // challenge
-        .ele('ChallengeRenderInterface').txt('01').up()
+        .ele('ChallengeRenderInterface').txt('03').up()
         .ele('ChallengeRenderType').txt('01').up()
         .ele('ChallengeRenderType').txt('02').up()
         .ele('ChallengeRenderType').txt('03').up()
-        // .ele('ChallengeRenderType').txt('04').up()
-        // .ele('ChallengeRenderType').txt('05').up()
+        .ele('ChallengeRenderType').txt('04').up()
+        .ele('ChallengeRenderType').txt('05').up()
       .up();
 
     const xml = root.end();
@@ -84,7 +83,7 @@ const processPayment = async (req = request, res = response) => {
     const formData = new URLSearchParams();
     formData.append('cmpi_msg', xml);
     
-    const response = await axios.post('', formData, {
+    const response = await axios.post('https://centineltest.cardinalcommerce.com/maps/txns.asp', formData, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
